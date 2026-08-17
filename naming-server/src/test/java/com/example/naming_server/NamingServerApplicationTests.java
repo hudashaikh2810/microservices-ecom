730e755769e92d0e605082e346b0903ec1b8b624
huda2810 <shaikhhuda2810@gmail.com>
2026-08-17 19:02:34 +0530

    Removed the SpringBootTest from NamingServerApplicationTest

--- naming-server/src/test/java/com/example/naming_server/NamingServerApplicationTests.java
+++ naming-server/src/test/java/com/example/naming_server/NamingServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.naming_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class NamingServerApplicationTests {
 
 	@Test
