131a337aee0a4c080de819492a3feb519ba92afa
huda2810 <shaikhhuda2810@gmail.com>
2026-09-18 22:02:49 +0530

    Dockerize the rate-limiter

--- config-server/src/test/java/com/example/config_server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/config_server/ConfigServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.config_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ConfigServerApplicationTests {
 
 	@Test
