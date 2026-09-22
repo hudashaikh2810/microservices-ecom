0b7b966ba7ff9468df55c595b23a8ccc1526554b
huda2810 <shaikhhuda2810@gmail.com>
2026-09-22 18:50:19 +0530

    Commented  the integration test file not needed for this non lua version

--- rate-limiter/src/test/java/com/example/rate_limiter/TockenBucketStrategyConcurrenyTest.java
+++ rate-limiter/src/test/java/com/example/rate_limiter/TockenBucketStrategyConcurrenyTest.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TokenBucketStrategyConcurrencyTest {
+
 /*
     @Autowired
     private TokenBucketStrategy tokenBucketStrategy;
